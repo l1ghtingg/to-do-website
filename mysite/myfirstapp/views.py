da47eb3f4a1f57504b5fe0e37b5ac18e1ace85ef
l1ghtingg <tuma62rus@gmail.com>
2025-08-14 01:06:53 +0300

    to-do website with work model

--- mysite/myfirstapp/views.py
+++ mysite/myfirstapp/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 class TaskList(ListView):
     model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = "myfirstapp/task.html"
