b88a5a4970d2b399be7df9a74334ea5a598760bd
l1ghtingg <tuma62rus@gmail.com>
2025-08-14 14:00:31 +0300

    model is working now!

--- mysite/myfirstapp/views.py
+++ mysite/myfirstapp/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from .models import Task
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
@@ -11,3 +14,19 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = "myfirstapp/task.html"
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
